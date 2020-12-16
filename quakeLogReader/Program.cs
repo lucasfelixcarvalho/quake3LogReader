9b0bba5c32126c15a36898af08bcec80889d6ce5
Lucas Felix Carvalho <lucas.felixcarvalho@hotmail.com>
2020-12-16 16:26:32 -0300

    Initial project commit

---
+++ quakeLogReader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace quakeLogReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Quake 3 Arena log reader!");
+        }
+    }
+}
